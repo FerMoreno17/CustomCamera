ac010a56f28c397fadf757da1dab122cb113d098
Fernando Moreno <fmoreno@gyf.com>
2022-05-26 19:54:08 -0300

    primeras pruebas

--- src/Camera.component.tsx
+++ src/Camera.component.tsx
@@ -1,16 +1,88 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
-import { Pressable, SafeAreaView, Text } from 'react-native'
-import React from 'react'
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React, { useRef, useState } from 'react'
+import { RNCamera } from 'react-native-camera';
 
 export default function Camera({ navigation }) {
+    let camera = useRef(null);
+    const [box, setBox] = useState(null);
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            flexDirection: 'column',
+            backgroundColor: 'black',
+        },
+        preview: {
+            flexGrow: 1,
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+        },
+        capture: {
+            flex: 0,
+            backgroundColor: '#fff',
+            borderRadius: 5,
+            padding: 15,
+            paddingHorizontal: 20,
+            alignSelf: 'center',
+            margin: 20,
+        },
+    });
+    async function takePicture() {
+        if (camera) {
+            const options = { quality: 0.5, base64: true };
+            const data = await camera.current?.takePictureAsync(options);
+            navigation.navigate('Preview', { image: data?.uri });
+        }
+    };
+
+    const handleFaceDetection = ({ faces }: any) => {
+        if (faces && faces[0]) {
+            console.log("Bounds==>", faces[0].bounds.origin);
+            console.log("Size==>", faces[0].bounds.size);
+            console.log("Roll==>", faces[0].rollAngle);
+            console.log("yaw==>", faces[0].yawAngle);
+            console.log("--->",faces);
+        }
+    };
+
     return (
-        <SafeAreaView style={{padding:20}}>
-            <Text style={{marginBottom:40}}>Camera</Text>
-            <Pressable
-                style={{backgroundColor:"blue", borderRadius:10, padding:20, maxWidth:100}}
-                onPress={() => navigation.navigate("Preview")}>
-                <Text style={{color:"white"}}>Preview</Text>
-            </Pressable>
+        <SafeAreaView style={{ flex: 1 }}>
+            <RNCamera
+                ref={camera}
+                style={styles.preview}
+                type={RNCamera.Constants.Type.front}
+                flashMode={RNCamera.Constants.FlashMode.on}
+                captureAudio={false}
+                faceDetectionClassifications={RNCamera.Constants.FaceDetection.Classifications.all}
+                faceDetectionLandmarks={RNCamera.Constants.FaceDetection.Landmarks.all}
+                onFacesDetected={handleFaceDetection}
+                androidCameraPermissionOptions={{
+                    title: 'Permission to use camera',
+                    message: 'We need your permission to use your camera',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+                androidRecordAudioPermissionOptions={{
+                    title: 'Permission to use audio recording',
+                    message: 'We need your permission to use your audio',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+            />
+            <View style={
+                {
+                    flex: 0,
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    backgroundColor: 'orange',
+                }
+            }>
+                <Pressable onPress={takePicture} style={styles.capture}>
+                    <Text style={{ fontSize: 14 }}> SNAP </Text>
+                </Pressable>
+            </View>
         </SafeAreaView>
-    )
+    );
 }
--- src/Preview.component.tsx
+++ src/Preview.component.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable prettier/prettier */
-import { Text, SafeAreaView, Pressable } from 'react-native'
+import { Text, SafeAreaView, Pressable, Image } from 'react-native'
 import React from 'react'
 
-export default function Preview({navigation}) {
+export default function Preview({navigation, route}) {
+    const {image} = route.params;
+
     return (
         <SafeAreaView style={{ padding: 20 }}>
             <Text style={{ marginBottom: 40 }}>Preview</Text>
+            <Image source={{uri:image}} style={{width:200, height:200}}/>
             <Pressable
                 style={{ backgroundColor: "red", borderRadius: 10, padding: 20, maxWidth: 100 }}
                 onPress={() => navigation.navigate("Camera")}>
